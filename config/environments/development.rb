d28d14247194ed23d16fee52ee22e6f914b30e9e
soreddysirish <ssireeshreddy@gmail.com>
2017-04-27 12:53:59 +0530

    changed mailing config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '107.170.30.178' }
 
 
   # Enable/disable caching. By default caching is disabled.
